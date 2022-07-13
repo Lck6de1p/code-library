091af5d0f22da40596698a16359f2aa8cd34b8c9
linchuanke <310504719@qq.com>
2022-07-13 13:56:57 +0800

    updateFormDataByKey

--- src/commonFn/index.js
+++ src/commonFn/index.js
@@ -1,7 +1,38 @@
-
 export const add = (a, b) => {
   return a + b;
 };
 
-
-
+/**
+ *
+ * @param {*} formData 操作对象
+ * @param {*} key 操作对象key值当key中存在“.”，需要将value赋给底层 如：key为"key1,0,key2",将会把值赋给formData["key1"]["0"]["key2"]
+ * @param {*} value key对应的value值
+ * @returns
+ */
+export const updateFormDataByKey = (formData, key, value) => {
+  /**
+   *  当key中存在“.”，需要将value赋给底层
+   * 如：key为"key1,0,key2",将会把值赋给formData["key1"]["0"]["key2"]
+   * 浅拷贝解决
+   */
+  if (key.indexOf(".") !== -1) {
+    const keys = key.split(".");
+    const k = keys.shift();
+    formData[k] = formData[k] || (isNaN(Number(keys[0])) ? {} : []);
+    let temp = formData[k];
+    while (keys.length) {
+      const k = keys.shift();
+      if (!keys.length) {
+        temp[k] = value;
+      } else {
+        if (!temp[k]) {
+          temp[k] = isNaN(Number(keys[0])) ? {} : [];
+        }
+        temp = temp[k];
+      }
+    }
+  } else {
+    formData[key] = value;
+  }
+  return formData;
+};
--- src/commonFn/tests/index.spec.js
+++ src/commonFn/tests/index.spec.js
@@ -1,5 +1,25 @@
-import { add } from "../index.js";
+import { add, updateFormDataByKey } from "../index.js";
 
 it("test", () => {
   expect(add(1, 2)).toBe(3);
+});
+
+describe("更新对象中的某个key值", () => {
+  it("happy path", () => {
+    const obj = {};
+    updateFormDataByKey(obj, "a", "a");
+    expect(obj.a).toBe("a");
+    updateFormDataByKey(obj, "arr.0.name", "john");
+    updateFormDataByKey(obj, "arr.0.age", "12");
+    expect(obj.arr[0].name).toBe("john");
+    expect(obj.arr[0].age).toBe("12");
+    expect(obj.a).toBe("a");
+    updateFormDataByKey(obj, "arr.1.name", "tom");
+    updateFormDataByKey(obj, "arr.1.age", "20");
+    expect(obj.arr[0].name).toBe("john");
+    expect(obj.arr[0].age).toBe("12");
+    expect(obj.arr[1].name).toBe("tom");
+    expect(obj.arr[1].age).toBe("20");
+    expect(obj.a).toBe("a");
+  });
 });

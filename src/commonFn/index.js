2883403b90fc6ef2f4cf5653c65900110b499c91
linchuanke <310504719@qq.com>
2022-07-18 15:31:16 +0800

    最长递增序列

--- src/commonFn/index.js
+++ src/commonFn/index.js
@@ -36,3 +36,53 @@ export const updateFormDataByKey = (formData, key, value) => {
   }
   return formData;
 };
+
+/**
+ * 最长递增序列
+ * @param {*} arr 
+ * @returns 最长递增序列下标index
+ */
+export const getSequence = (arr) => {
+  const p = arr.slice(); //  保存原始数据
+  const result = [0]; //  存储最长增长子序列的索引数组
+  let i, j, u, v, c;
+  const len = arr.length;
+  for (i = 0; i < len; i++) {
+    const arrI = arr[i];
+    if (arrI !== 0) {
+      j = result[result.length - 1]; //  j是子序列索引最后一项
+      if (arr[j] < arrI) {
+        //  如果arr[i] > arr[j], 当前值比最后一项还大，可以直接push到索引数组(result)中去
+        p[i] = j; //  p记录第i个位置的索引变为j
+        result.push(i);
+        continue;
+      }
+      u = 0; //  数组的第一项
+      v = result.length - 1; //  数组的最后一项
+      while (u < v) {
+        //  如果arrI <= arr[j] 通过二分查找，将i插入到result对应位置；u和v相等时循环停止
+        c = (u + v) >> 1; //  二分查找
+        if (arr[result[c]] < arrI) {
+          u = c + 1; //  移动u
+        } else {
+          v = c; //  中间的位置大于等于i,v=c
+        }
+      }
+      if (arrI < arr[result[u]]) {
+        if (u > 0) {
+          p[i] = result[u - 1]; //  记录修改的索引
+        }
+        result[u] = i; //  更新索引数组(result)
+      }
+    }
+  }
+  u = result.length;
+  v = result[u - 1];
+  //把u值赋给result
+  while (u-- > 0) {
+    //  最后通过p数组对result数组进行进行修订，取得正确的索引
+    result[u] = v;
+    v = p[v];
+  }
+  return result;
+};
--- src/commonFn/tests/index.spec.js
+++ src/commonFn/tests/index.spec.js
@@ -1,4 +1,4 @@
-import { add, updateFormDataByKey } from "../index.js";
+import { add, updateFormDataByKey, getSequence } from "../index.js";
 
 it("test", () => {
   expect(add(1, 2)).toBe(3);
@@ -22,4 +22,14 @@ describe("更新对象中的某个key值", () => {
     expect(obj.arr[1].age).toBe("20");
     expect(obj.a).toBe("a");
   });
+});
+
+describe("最长递增序列", () => {
+  const result = getSequence([7, 7, 7, 7, 7]);
+  expect(result).toEqual([0]);
+
+  const result2 = getSequence([
+    0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15,
+  ]);
+  expect(result2).toEqual([0, 4, 6, 9, 13, 15]);
 });
